ccb1a859682c6cfc97eac3f759204febf712ad5d
Chris Athanas <chris@crowdskout.com>
2021-09-03 13:42:02 -0500

    Website update.

--- export.sh
+++ export.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cd $1
+cd /Users/chrisathanas/Dropbox\ \(Personal\)/My\ Mac\ \(Chriss-MBP.attlocal.net\)/Desktop/dev/Bootstrap\ Upload\ to\ Github/realityexpander.github.io
+
 git add -A
 git commit -m "Website update."
 git push origin master3
 
+
+git status >> ../error.log
+pwd >> ../error.log
+ 
